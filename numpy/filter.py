87aae4a13afe230b2441761f94dc6a149d84ea9f
Mudra111 <mudrabhalodiya@gmail.com>
2026-03-03 23:59:03 +0530

    filtering in numpy array

---
+++ numpy/filter.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6,7,8,9,10])
+
+# Using boolean list
+x = [True, False, False, True, True, False,True,True,False,True]
+print(arr)
+print(arr[x])
+
+# Using for loop and boolean array
+y = []
+for i in arr:
+    if i <= 5:
+        y.append(True)
+    else:
+        y.append(False)
+
+print(arr)
+print(y)
+print(arr[y])
+
+# Using numpy shortcut
+filter = arr > 6 
+print(arr)
+print(filter)
+print(arr[filter])
