ca5342c465a1c46eda4826e3a312ee62182c142e
Mudra111 <mudrabhalodiya@gmail.com>
2026-05-09 13:04:39 +0530

    complete heart stroke prediction app

--- heart_disease_project/app.py
+++ heart_disease_project/app.py
@@ -23,11 +23,31 @@ oldpeak = st.slider('Oldpeak (ST Depression)',0.0, 6.0, 1.0)
 st_slope = st.selectbox('ST Slope',['Up','Flat','Down'])
 
 if st.button('Predict'):
-    raw_imput = {
+    raw_input = {
         'Age':age,
         'RestingBP':restingBp,
         'Cholesterol':cholesterol,
         'FastingBS':fasting_bs,
         'MaxHR':max_hr,
         'Oldpeak':oldpeak,
+        'is_female' : 1 if sex == 'F' else 0,
+        'ChestPainType_' + chest_pain : 1,
+        'RestingECG_' + resting_ecg : 1,
+        'ST_Slope_' + st_slope : 1
     }
+
+    input_df = pd.DataFrame([raw_input])
+
+    for col in expected_columns:
+        if col not in input_df.columns:
+            input_df[col] = 0
+
+    input_df = input_df[expected_columns]
+
+    scaled_input = scaler.transform(input_df)
+    prediction = model.predict(scaled_input)[0]
+
+    if prediction == 1:
+        st.error('High Risk of Heart Disease')
+    else:
+        st.success('Low Risk of Heart Disease')
